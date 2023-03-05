e489ada3fcbfcfa177e1b4aa37cd63368e38de77
cse-child <99868973+cse-child@users.noreply.github.com>
2023-03-05 23:26:52 +0900

    # Add start file

---
+++ 230223-2_Start_Cpp_Project/UEProject_03/Source/UEProject_03/01_Spawn/C01_Property.cpp
@@ -0,0 +1,20 @@
+#include "01_Spawn/C01_Property.h"
+
+AC01_Property::AC01_Property()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AC01_Property::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AC01_Property::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ 230223-2_Start_Cpp_Project/UEProject_03/Source/UEProject_03/UEProject_03.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEProject_03.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEProject_03, "UEProject_03" );
