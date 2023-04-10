bcfbd7c5da6e54bf0b9e865358dbe0f9be553747
cse-child <99868973+cse-child@users.noreply.github.com>
2023-04-10 15:12:02 +0900

    # 230410 study

---
+++ 230410_Animation/UEProject_06/Source/UEProject_06/Characters/CPlayer.cpp
@@ -0,0 +1,68 @@
+#include "Characters/CPlayer.h"
+#include "Global.h"
+#include "CAnimInstance.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CMovementComponent.h"
+#include "Components/CStateComponent.h"
+#include "Components/CMontagesComponent.h"
+#include "Components/InputComponent.h"
+
+ACPlayer::ACPlayer()
+{
+	/* Component 추가 */
+	CHelpers::CreateComponent<USpringArmComponent>(this, &SpringArm, "SpringArm", GetMesh());
+	CHelpers::CreateComponent<UCameraComponent>(this, &Camera, "Camera", SpringArm);
+
+	CHelpers::CreateActorComponent<UCMovementComponent>(this, &Movement, "Movement");
+	CHelpers::CreateActorComponent<UCStateComponent>(this, &State, "State");
+	CHelpers::CreateActorComponent<UCMontagesComponent>(this, &Montages, "Montages");
+	
+
+	/* Mesh */
+	GetMesh()->SetRelativeLocation(FVector(0, 0, -90));
+	GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
+
+	USkeletalMesh* mesh;
+	CHelpers::GetAsset<USkeletalMesh>(&mesh, "SkeletalMesh'/Game/Character/Mesh/SK_Mannequin.SK_Mannequin'");
+	GetMesh()->SetSkeletalMesh(mesh);
+
+	/* Anim Instance */
+	TSubclassOf<UCAnimInstance> animInstance;
+	CHelpers::GetClass< UCAnimInstance>(&animInstance, "AnimBlueprint'/Game/ABP_Character.ABP_Character_C'");
+	GetMesh()->SetAnimClass(animInstance);
+
+	/* Spring Arm 초기화 */
+	SpringArm->SetRelativeLocation(FVector(0, 0, 140));
+	SpringArm->SetRelativeRotation(FRotator(0, 90, 0));
+	SpringArm->TargetArmLength = 200;
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->bEnableCameraLag = true;
+
+	GetCharacterMovement()->RotationRate = FRotator(0, 720, 0);
+}
+
+void ACPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Movement->SetSpeed(ESpeedType::Run);
+	Movement->DisableControlRotation();
+}
+
+
+void ACPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", Movement, &UCMovementComponent::OnMoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", Movement, &UCMovementComponent::OnMoveRight);
+	PlayerInputComponent->BindAxis("HorizontalLook", Movement, &UCMovementComponent::OnHorizontalLook);
+	PlayerInputComponent->BindAxis("VertocalLook", Movement, &UCMovementComponent::OnHorizontalLook);
+
+	PlayerInputComponent->BindAction("Sprint", EInputEvent::IE_Pressed, Movement, &UCMovementComponent::OnSprint);
+	PlayerInputComponent->BindAction("Run", EInputEvent::IE_Pressed, Movement, &UCMovementComponent::OnRun);
+}
+
