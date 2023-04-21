af70ad6302c164790f88ab9d7d4847d1728fd347
cse-child <99868973+cse-child@users.noreply.github.com>
2023-04-21 17:47:05 +0900

    # 230421 study

---
+++ 230421_Sound_Effect/UEProject_06/Source/UEProject_06/Weapons/CAttachment.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CAttachment.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FAttachmentBeginCollision);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FAttachmentEndCollision);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FAttachmentBeginOverlap, class ACharacter*, InAttacker, AActor*, InAttackCuaser, class ACharacter*, InOther);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FAttachmentEndOverlap, class ACharacter*, InAttacker, class ACharacter*, InOther);
+
+UCLASS()
+class UEPROJECT_06_API ACAttachment : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+		class USceneComponent* Root;
+
+public:	
+	ACAttachment();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintImplementableEvent)
+		void OnBeginEquip();
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void OnUnequip();
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Attach")
+		void AttachTo(FName InSocketName);
+
+private:
+	UFUNCTION()
+		void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:
+	void OnCollision();
+	void OffCollision();
+
+public:
+	FAttachmentBeginCollision OnAttachmentBeginCollision;
+	FAttachmentEndCollision OnAttachmentEndCollision;
+
+	FAttachmentBeginOverlap OnAttachmentBeginOverlap;
+	FAttachmentEndOverlap OnAttachmentEndOverlap;
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+		class ACharacter* OwnerCharacter;
+
+protected:
+	TArray<class UShapeComponent*> Collisions;
+
+
+};
+
+
